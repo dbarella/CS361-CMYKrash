99bfd5a388e9ec66810b5235a281fa6b9ab4bc8a
Adam Stafford <Astaffor@Oberlin.edu>
2012-11-14 02:31:34 -0500

    Modify Weapon.cs, add Cannon.cs, Machinegun.cs, PowerShot.cs
    These changes are currently untested - I'm not sure how to test them
    within the current environment as I don't think have the rest of the
    required scripts.

---
+++ Assets/Scripts/Weapons/Cannon.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Cannon : Weapon {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update();
+	}
+	
+	override public void PowerShot(){
+		//pass for now
+	}
+}
