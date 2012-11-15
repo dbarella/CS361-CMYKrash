10697994dfbc04ff67ca7f532a7de23d64a9d9b3
emmaconner <econner@oberlin.edu>
2012-11-14 21:19:40 -0500

    Added Adam's YAM and Laser scripts

---
+++ Assets/Scripts/Weapons/YAM.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class YAM : Weapon{
+
+// Use this for initialization
+void Start () {
+
+}
+
+// Update is called once per frame
+	void Update () {
+		base.Update();
+	}
+
+	public void PowerShot(){
+		//pass for now
+	}
+}
+
+
+}
+
