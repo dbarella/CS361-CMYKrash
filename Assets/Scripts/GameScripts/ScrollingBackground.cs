3f682f3860097517d3adbdf1cd6e9b616dfea292
Daniel Barella <dan.barella@gmail.com>
2012-12-02 16:21:29 -0500

    Added Josh's Scrolling Background.

--- Assets/Scripts/GameScripts/ScrollingBackground.cs
+++ Assets/Scripts/GameScripts/ScrollingBackground.cs
@@ -4,22 +4,35 @@ using System.Collections;
 public class ScrollingBackground : MonoBehaviour {
 	 
 	protected GameManagement mgmt;
-
-	// Use this for initialization
+	
+	private float rightBound;
+	private float leftBound;
+	private float screenWidth;
+	
+	public float scrollSpeed=0.5f;
+	public Texture2D bumpMap;
+	
 	void Start () {
 		mgmt = Camera.main.GetComponent<GameManagement>();
 		Scale();
-	
+		rightBound = Camera.main.ScreenToWorldPoint( new Vector3(0, Camera.main.GetScreenWidth(), 0) ).x;
+		leftBound = -rightBound;
+		screenWidth = 2.0f*rightBound;
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	// In order for this to scroll in accordance with player movements you'll need to go to
+	//Edit->project settings -> input
+	void FixedUpdate () {
+		float moveCompound = Input.GetAxis("Horizontal") *0.1f;
+		float offset = Time.time * scrollSpeed*0.1f;
+		renderer.material.mainTextureOffset = new Vector2(-offset,0);
 	}
 	
 	//scales the object assuming it is instantiated at (0,0,20)
 	void Scale(){
-		float x = mgmt.GetScreenWidth()/2.0f;
-		float y = mgmt.GetScreenHeight()/2.0f;
+		float x = mgmt.GetScreenWidth()/5.0f;
+		float y = mgmt.GetScreenHeight()/7.0f;
 		Vector3 rescale = new Vector3(x,0,y);
 		transform.localScale = rescale;
 	}
